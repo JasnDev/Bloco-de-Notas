637a237aa5eaa9d813177b9c050d4030e1dc11d5
JasnDev <jasaniastadeu@gmail.com>
2024-05-21 14:00:28 -0300

    FireBase

---
+++ src/firebase.jsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCQfGN1r3ZDReQ1K-1LfcAVAwlgOFIpzt0",
+  authDomain: "listareact-8d567.firebaseapp.com",
+  projectId: "listareact-8d567",
+  storageBucket: "listareact-8d567.appspot.com",
+  messagingSenderId: "970566520658",
+  appId: "1:970566520658:web:2c63dbc6431dc5edb6b48b",
+  measurementId: "G-J5XKMP5ETN"
+};
+
+export default { firebaseConfig } 
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,14 @@ import Title from './components/title.jsx';
 import CheckBox from './components/checkBox.jsx';
 import ListItems from './components/listItemDeleteButton.jsx';
 import App from './App.jsx';
-import List from './components/listMain.jsx'
+import List from './components/listMain.jsx';
+import { firebaseConfig } from '/home/jass/Bloco-de-Notas/src/firebase.jsx'
+import {  initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+import { getFirestore, Timestamp, FieldValue, Filter } from 'firebase-admin/firestore';
 import './index.css';
+
+initializeApp();
+const db = getFirestore();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
